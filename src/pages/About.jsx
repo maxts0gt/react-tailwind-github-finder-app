a96bc4857e26d052b5a8d495bb50f5223bee486d
Max <bazarsadtsogtjargal@gmail.com>
2022-01-05 23:26:59 +0900

    add pages and routes

---
+++ src/pages/About.jsx
@@ -0,0 +1,14 @@
+function About() {
+	return (
+		<>
+			<h1 className='mb-4 text-6xl'>Github Finder</h1>
+			<p className='mb-4 text-2xl font-light'>
+				Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quia
+				sunt tempora odio? Ipsa molestiae consequatur ullam aspernatur
+				consequuntur? Reiciendis, sapiente.
+			</p>
+		</>
+	);
+}
+
+export default About;
