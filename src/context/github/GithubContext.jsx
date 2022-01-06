cf9a86daabf06a49122bf9f90d840fa528e6b419
Max <bazarsadtsogtjargal@gmail.com>
2022-01-06 13:59:43 +0900

    clear users

--- src/context/github/GithubContext.jsx
+++ src/context/github/GithubContext.jsx
@@ -34,11 +34,20 @@ export const GithubProvider = ({ children }) => {
 		});
 	};
 
+	const clearUsers = () => {
+		dispatch({ type: 'CLEAR_USERS' });
+	};
+
 	const setLoading = () => dispatch({ type: 'SET_LOADING' });
 
 	return (
 		<GithubContext.Provider
-			value={{ users: state.users, loading: state.loading, searchUsers }}
+			value={{
+				users: state.users,
+				loading: state.loading,
+				searchUsers,
+				clearUsers,
+			}}
 		>
 			{children}
 		</GithubContext.Provider>
