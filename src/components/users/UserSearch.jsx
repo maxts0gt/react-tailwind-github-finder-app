e678307eeb7d8b823c0d80becc42ed4b54bac1c0
Max <bazarsadtsogtjargal@gmail.com>
2022-01-06 13:50:27 +0900

    enable search functionality

--- src/components/users/UserSearch.jsx
+++ src/components/users/UserSearch.jsx
@@ -4,7 +4,7 @@ import GithubContext from '../../context/github/GithubContext';
 function UserSearch() {
 	const [text, setText] = useState('');
 
-	const { users, dispatch } = useContext(GithubContext);
+	const { users, searchUsers } = useContext(GithubContext);
 
 	const handleChange = (e) => setText(e.target.value);
 
@@ -14,7 +14,7 @@ function UserSearch() {
 		if (text === '') {
 			alert('Please enter a text');
 		} else {
-			// @todo - search users
+			searchUsers(text);
 			setText('');
 		}
 	};
@@ -44,12 +44,7 @@ function UserSearch() {
 			</div>
 			{users.length > 0 && (
 				<div>
-					<button
-						onClick={() => dispatch({ type: 'CLEAR_USERS' })}
-						className='btn btn-ghost btn-lg'
-					>
-						Clear
-					</button>
+					<button className='btn btn-ghost btn-lg'>Clear</button>
 				</div>
 			)}
 		</div>
