2b2a08c6eaabbb7ff26e0d9a882c5a01782ca9cd
Max <bazarsadtsogtjargal@gmail.com>
2022-01-06 15:54:51 +0900

    create alert component

--- src/App.jsx
+++ src/App.jsx
@@ -5,24 +5,30 @@ import NotFound from './pages/NotFound';
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
 import { GithubProvider } from './context/github/GithubContext';
+import { AlertProvider } from './context/alert/AlertContext';
 
 function App() {
 	return (
 		<GithubProvider>
-			<Router>
-				<div className='flex flex-col justify-between h-screen'>
-					<Navbar />
-					<main className='container px-3 pb-12 mx-auto'>
-						<Routes>
-							<Route path='/' element={<Home />} />
-							<Route path='/about' element={<About />} />
-							<Route path='/notfound' element={<NotFound />} />
-							<Route path='/*' element={<NotFound />} />
-						</Routes>
-					</main>
-					<Footer />
-				</div>
-			</Router>
+			<AlertProvider>
+				<Router>
+					<div className='flex flex-col justify-between h-screen'>
+						<Navbar />
+						<main className='container px-3 pb-12 mx-auto'>
+							<Routes>
+								<Route path='/' element={<Home />} />
+								<Route path='/about' element={<About />} />
+								<Route
+									path='/notfound'
+									element={<NotFound />}
+								/>
+								<Route path='/*' element={<NotFound />} />
+							</Routes>
+						</main>
+						<Footer />
+					</div>
+				</Router>
+			</AlertProvider>
 		</GithubProvider>
 	);
 }
