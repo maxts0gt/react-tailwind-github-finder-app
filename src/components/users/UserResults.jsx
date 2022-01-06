a0aa81aa389989f56835269de1934dac2babf9a4
Max <bazarsadtsogtjargal@gmail.com>
2022-01-06 12:39:25 +0900

    display users

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import Spinner from '../layout/Spinner';
+import UserItem from '../users/UserItem';
 
 function UserResults() {
 	const [users, setUsers] = useState([]);
@@ -26,8 +27,8 @@ function UserResults() {
 	if (!loading) {
 		return (
 			<div className='grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2'>
-				{users.map((user, index) => (
-					<h3>{user.login}</h3>
+				{users.map((user) => (
+					<UserItem key={user.id} user={user} />
 				))}
 			</div>
 		);
