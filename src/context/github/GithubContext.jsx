fdfc7630e27123d5f5724c29ff62a2713bee1d16
Max <bazarsadtsogtjargal@gmail.com>
2022-01-10 18:13:33 +0900

    create user stats page

--- src/context/github/GithubContext.jsx
+++ src/context/github/GithubContext.jsx
@@ -9,6 +9,7 @@ const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
 export const GithubProvider = ({ children }) => {
 	const initialState = {
 		users: [],
+		user: {},
 		loading: false,
 	};
 
@@ -34,6 +35,28 @@ export const GithubProvider = ({ children }) => {
 		});
 	};
 
+	// get a single user
+	const getUser = async (login) => {
+		setLoading();
+
+		const response = await fetch(`${GITHUB_URL}/users/${login}`, {
+			headers: {
+				Authorization: `token ${GITHUB_TOKEN}`,
+			},
+		});
+
+		if (response.status === 404) {
+			window.location = '/notfound';
+		} else {
+			const data = await response.json();
+
+			dispatch({
+				type: 'GET_USER',
+				payload: data,
+			});
+		}
+	};
+
 	const clearUsers = () => {
 		dispatch({ type: 'CLEAR_USERS' });
 	};
@@ -45,8 +68,10 @@ export const GithubProvider = ({ children }) => {
 			value={{
 				users: state.users,
 				loading: state.loading,
+				user: state.user,
 				searchUsers,
 				clearUsers,
+				getUser,
 			}}
 		>
 			{children}
